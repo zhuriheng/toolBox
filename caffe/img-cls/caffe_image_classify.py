5a861dd7f1c294563fed7eab6b80329b218fb0c0
zhuriheng <zrh0712@outlook.com>
2018-07-02 21:46:52 +0800

    update caffe_image_classify.py

--- caffe/img-cls/caffe_image_classify.py
+++ caffe/img-cls/caffe_image_classify.py
@@ -10,6 +10,7 @@ from collections import OrderedDict
 
 import cv2
 import numpy as np
+import urllib
 
 sys.path.insert(0, 'caffe/python')
 import caffe
@@ -80,7 +81,7 @@ def label_correspond(label_corres_list, dict_results):
     return dict_results
 
 
-def single_img_process(net_cls, img_path, ori_img, label_list):
+def single_img_process(net_cls, img, label_list):
     """
     docstring here
         :param net_cls: 
@@ -88,7 +89,7 @@ def single_img_process(net_cls, img_path, ori_img, label_list):
         :param ori_img: 
         :param label_list: 
     """
-    img = cv2.imread(os.path.join(img_path, ori_img))
+    #img = cv2.imread(os.path.join(img_path, ori_img))
     if np.shape(img) != () and np.shape(img)[2] != 4:
         start_time = time.time()
         img = img.astype(np.float32, copy=True)
@@ -110,31 +111,31 @@ def single_img_process(net_cls, img_path, ori_img, label_list):
         index_list = output_prob.argsort()
         rate_list = output_prob[index_list]
 
-        list_result = []
         result_dict = OrderedDict()
-        result_dict['File Name'] = ori_img
         result_dict['Top-1 Index'] = index_list[-1]
         result_dict['Top-1 Class'] = label_list[int(index_list[-1])].split(' ')[1]
 
         result_dict['Confidence'] = [str(i) for i in list(output_prob)]
-        list_result.append(result_dict)
+        return result_dict
+    print '*'*40 + ' Error image ' + '*'*40 
+    return None
 
-    return list_result
 
-def generate_rg_results(dict_results, output):
+def generate_rg_results(dict_results, threshold, output):
     """
     docstring here
         :param dict_results: 
+        :param threshold: 
         :param output: 
     """
     # 回归测试48类到6类的类别映射表
     labels = ['bloodiness', 'bomb', 'beheaded', 'march', 'fight', 'normal']
-    map = {'0': [0],
-           '1': [1, 2, 3, 4],
-           '2': [5, 6],
-           '3': [7, 8],
-           '4': [9, 10]}  # 暴恐5类
-    map['5'] = list(range(11, 48))  # 正常18到47类
+    map = {'0': [0],  # 'bloodiness'
+           '1': [1, 2, 3, 4],  # 'bomb'
+           '2': [5, 6],  # 'beheaded'
+           '3': [7, 8],  # 'march' 
+           '4': [9, 10],  # 'fight'
+           '5': list(range(11, 48))}  # 'normal'
 
     with open(output, 'w') as fo:
         for (img_name, results) in dict_results.iteritems():
@@ -142,13 +143,10 @@ def generate_rg_results(dict_results, output):
             img_name = os.path.split(img_name)[-1]
             index = int(results['Top-1 Index'])
             prob = float(results['Confidence'][index])
-            # 获取label信息
-            for key in map.keys():
-                if index in map[key]:
-                    cls_name = labels[key]
-                    break
-            # 暴恐分类目前线上的逻辑
-            if prob < 0.9 and cls_name != 'normal':
+            # 获取对应的label
+            [cls_name] = [labels[int(key)] for key in map.keys() if index in map[key]]
+            # 暴恐分类目前的线上逻辑: 只有类别是非normal，并且score小于0.9的才被标为reviewer
+            if prob < threshold and cls_name != 'normal':
                 cls_name = 'normal'
                 index = -1
 
@@ -163,18 +161,63 @@ def generate_rg_results(dict_results, output):
     print "Generate %s with success" % (output)
 
 
+def process_single_img(img_path, net_cls, label_list):
+    dict_results = OrderedDict()
+    start_time = time.time()
+    img = cv2.imread(img_path)
+    dict_result = single_img_process(net_cls, img, label_list)
+    end_time = time.time()
+    print('Inference speed: {:.3f}s / iter'.format(end_time - start_time))
+    dict_result.update({'File Name': img_path})
+    dict_results[os.path.basename(img_path)] = dict_result
+    return dict_results
+
+
+def process_img_list(root, img_list_path, net_cls, label_list):
+    img_list = np.loadtxt(img_list_path, str, delimiter='\n')
+    dict_results = OrderedDict()
+    for i in range(len(img_list)):
+        start_time = time.time()
+        img_path = os.path.join(root, img_list[i].split(' ')[0])
+        img = cv2.imread(img_path)
+        dict_result = single_img_process(net_cls, img, label_list)
+        end_time = time.time()
+        print('Inference speed: {:.3f}s / iter'.format(end_time - start_time))
+        dict_result.update({'File Name': img_path})
+        dict_results[os.path.basename(img_path)] = dict_result
+    return dict_results
+
+
+def process_img_urllist(url_list_path, prefix, net_cls, label_list):
+    url_list = np.loadtxt(url_list_path, str, delimiter='\n')
+    dict_results = OrderedDict()
+    for i in range(len(url_list)):
+        url = prefix + url_list[i] if prefix else url_list[i]
+        start_time = time.time()
+        img = urllib.urlopen(url).read()
+        img = np.fromstring(img, np.uint8)
+        img = cv2.imread(img, 1)
+        dict_result = single_img_process(net_cls, img, label_list)
+        end_time = time.time()
+        print('Inference speed: {:.3f}s / iter'.format(end_time - start_time))
+        dict_result.update({'File Name': url})
+        dict_results[os.path.basename(url)] = dict_result
+    return dict_results
+
 def parse_arg():
     parser = argparse.ArgumentParser(description='caffe image classify')
     parser.add_argument('--weight', help='caffemodel', type=str, required=True)
     parser.add_argument('--deploy', help='deploy.prototxt',type=str, required=True)
     parser.add_argument('--gpu', help='gpu id', type=int, required=True)
     parser.add_argument('--labels', help='labels list',type=str, required=True)
+    parser.add_argument('-thrs','--threshold', help='threshold for inference result',type=float, default=0.9)
     parser.add_argument('--labels_corres', help='labels correspond list', type=str, required=False)
-    parser.add_argument('--rg', help='add "--rg" to generate regression result',action='store_true')
-    parser.add_argument('--img_list', help='input image list',type=str, required=False)
-    parser.add_argument('--root', help='data root for image',type=str, required=False)
-    parser.add_argument('--url_list', help='input image url list', type=str, required=False)
+    parser.add_argument('-rg','--regression_result', help='add "--rg" to generate regression result',action='store_true')
     parser.add_argument('--img', help='input image, inference for single local image', type=str, required=False)
+    parser.add_argument('--img_list', help='input image list', default=None, type=str, required=False)
+    parser.add_argument('--root', help='data root for image', default=None, type=str, required=False)
+    parser.add_argument('--url_list', help='input image url list', default=None, type=str, required=False)
+    parser.add_argument('--prefix', help='prefix for image url', default=None, type=str, required=False)
     
     return parser.parse_args()
 
@@ -183,26 +226,25 @@ def main():
     now = datetime.datetime.now()
     
     net_cls = init_models(args.weight, args.deploy, args.gpu)
-
     label_list = np.loadtxt(args.labels, str, delimiter='\n')
-    img_list = np.loadtxt(args.img_list, str, delimiter='\n')
 
-    dict_results = {}
-
-    for i in range(len(img_list)):
-        start_time = time.time()
-        dict_result = single_img_process(
-            net_cls, args.root, img_list[i].split(' ')[0], label_list)
-        end_time = time.time()
-        print('Inference speed: {:.3f}s / iter'.format(end_time - start_time))
-        for item in dict_result:
-            dict_results[os.path.basename(item['File Name'])] = item
+    dict_results = OrderedDict()
+    if args.img:
+        # 推理单张图片
+        dict_results = process_single_img(args.img, net_cls, label_list)
+    elif args.img_list:
+        # 推理存储在本地的图片
+        dict_results = process_img_list(
+            args.root, args.img_list, net_cls, label_list)
+    elif args.url_list:
+        # 推理url形式的图片
+        dict_results = process_img_urllist(
+            args.url_list, args.prefix, net_cls, label_list)
     
-    if args.rg:
+    if args.regression_result:
         rg_output = os.path.join(args.root, 'regression_%s.tsv' % (now.strftime("%m%d%H%M")))
-        generate_rg_results(dict_results, rg_output)
+        generate_rg_results(dict_results, args.threshold, rg_output)
             
-
     if args.labels_corres:
         label_corres_list = np.loadtxt(args.labels_corres, str, delimiter='\n')
         dict_results = label_correspond(label_corres_list, dict_results)
